eeb5ac00a6c7cbfe1679a6ae44d4465b86ebe5f6
MeredithBrandon <meredithbran2754@yahoo.com>
2022-12-20 23:40:18 -0500

    add OnePost file

---
+++ frontend/src/views/OnePost.vue
@@ -0,0 +1,26 @@
+<template>
+    <HeaderLogo />
+    <RouterLink to="/home">
+        <button class="btn btn-primary btn-med" id="home_btn" role="button"><strong>Home</strong></button>
+    </RouterLink>
+    <OnePost />
+    <RouterView />
+</template>
+
+<script>
+import HeaderLogo from '@/components/HeaderLogo.vue';
+import OnePost from '@/components/OnePost.vue';
+
+export default {
+    data() {
+        return{
+            posts: []
+        }
+    },
+
+    components: {
+        HeaderLogo,
+        OnePost
+    }
+}
+</script>
