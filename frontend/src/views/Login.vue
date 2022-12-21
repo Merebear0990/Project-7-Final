58610d82b169499e75da0c4f2ef5aeae40597f08
MeredithBrandon <meredithbran2754@yahoo.com>
2022-12-20 23:41:39 -0500

    add Login file

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <LoginCard />
+    <router-view />
+</template>
+
+<script>
+import LoginCard from '../components/LoginCard.vue';
+
+export default {
+    name: 'login',
+    components: {
+        LoginCard
+    }
+}
+</script>
