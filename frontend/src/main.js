5490f9d0e6621693197db2a3882a5b735144d6d4
MeredithBrandon <meredithbran2754@yahoo.com>
2022-12-25 08:13:32 -0500

    import nodemon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/dist/js/bootstrap.bundle";
 
 createApp(App).mount('#app')
