147594665cca9c3d2ef61cc7a87354cd2834b1f9
MeredithBrandon <meredithbran2754@yahoo.com>
2023-01-29 06:55:04 -0500

    Making changes to the frontend browser style

--- frontend/src/components/LoginCard.vue
+++ frontend/src/components/LoginCard.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class = "home">
-    <div class = "home_page_img">
-      <img id = "logo" alt="groupomania logo" src="../assets/icons/icon-left-font-monochrome-white.png" />
-    </div> 
-    <div class = "login_section">
-      <div class="col-md-4" id = "login_card">
-        <form class = "login_form" id = "login_form">
-          <label for ="email-input" class = "form-label"></label>
-            <input type= "text" class="form-control" id = "email" placeholder="Email" v-model ="email" required/>
-          <label for = "password-input" class = "form-label"></label>
-            <input type="text" class="form-control" id = "password" placeholder ="Password" v-model ="password" required/>
+  <div class="home">
+    <div class="home_page_img">
+      <img id="logo" alt="groupomania logo" src="../assets/icons/icon-left-font-monochrome-white.svg" />
+    </div>
+    <div class="login_section">
+      <div class="col-md-4" id="login_card">
+        <form class="login_form" id="login_form">
+          <label for="email-input" class="form-label"></label>
+          <input type="text" class="form-control" id="email" placeholder="Email" v-model="email" required />
+          <label for="password-input" class="form-label"></label>
+          <input type="text" class="form-control" id="password" placeholder="Password" v-model="password" required />
         </form>
-          <router-link to ="/home">
-            <button type = "submit" v-on:click.prevent ="LogIn()" id = "login_btn">Log In</button>
-          </router-link>
-          <router-link to ='/signup'>
-            <button type = "submit"  id = "signup_btn">Create new account</button>
-          </router-link>
+        <router-link to="/home">
+          <button type="submit" v-on:click.prevent="LogIn()" id="login_btn">Log In</button>
+        </router-link>
+        <router-link to='/signup'>
+          <button type="submit" id="signup_btn">Create new account</button>
+        </router-link>
       </div>
     </div>
   </div>
@@ -26,40 +26,40 @@
 
 <script>
 export default {
- data() {
-  return {
-   email: "",
-   password: ""
-  };
- },
- methods: {
-  LogIn() {
-    const logInForm = {
-      email: this.email,
-      password: this.password,
+  data() {
+    return {
+      email: "",
+      password: ""
     };
-    const requestOptions = {
-      method: "POST",
-      headers: {
-      "Content-Type": "application/json",
-      },
-      body: JSON.stringify(logInForm),
-    };
-    fetch("http://localhost:3000/api/users/login", requestOptions)
-      .then((res) => {
-      return res
-        .json()
-        .then((data) => {
-        console.log(data);
-        if (res.ok) {
-          localStorage.setItem("user", JSON.stringify(data));
-          this.$router.push("/home");
-        } 
+  },
+  methods: {
+    LogIn() {
+      const logInForm = {
+        email: this.email,
+        password: this.password,
+      };
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(logInForm),
+      };
+      fetch("http://localhost:3000/api/users/login", requestOptions)
+        .then((res) => {
+          return res
+            .json()
+            .then((data) => {
+              console.log(data);
+              if (res.ok) {
+                localStorage.setItem("user", JSON.stringify(data));
+                this.$router.push("/home");
+              }
+            });
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
         });
-      })
-      .catch((error) => {
-      console.error("There was an error!", error);
-      });
     },
   },
 };
@@ -68,56 +68,71 @@ export default {
 
 <style scoped>
 * {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
 }
+
 .home {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100vw;
-    height: 100vh;
-    background-color: lightgrey;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(198, 194, 194);
 }
+
 .home_page_img {
-    display: inline-block;
-    justify-content: start;
-    height: 8rem;
-    width: 33%;
-    margin: 21.3rem 0 0 16.5rem;
+  display: inline-block;
+  justify-content: start;
+  height: 8rem;
+  width: 33%;
+  margin: 10rem 0 0 16.5rem;
+
 }
+
 #logo {
-    height: 5rem;
+  margin: 100px;
+  height: 5rem;
+  background-color: rgb(9, 48, 117);
+  opacity: 0.6;
+  border: 1px solid black;
+
 }
+
 .login_form {
-    display: flex;
-    flex-direction: column;
-    margin: 1rem 4rem 0 4rem;
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 4rem 0 4rem;
 }
-label, input {
-    padding: .5rem;
-    box-sizing: border-box;
-    text-align: center;
-    font-weight: 400;
-    font-size: medium;
+
+label,
+input {
+  padding: .5rem;
+  box-sizing: border-box;
+  text-align: center;
+  font-weight: 400;
+  font-size: medium;
 }
+
 .login_section {
-    display: inline-block;
-    justify-content: end;
-    width: 35%;
-    height: auto;
-    margin: 12rem 13rem 0 0;
+  display: inline-block;
+  justify-content: end;
+  width: 35%;
+  height: auto;
+  margin: 12rem 13rem 0 0;
 }
+
 #login_card {
-    background-color: #FBF9F9;
-    padding: 5px;
-    width: 100%;
-    height: 24rem;
-    margin-bottom: 10px;
-    box-shadow: 8px 8px 3px rgba(0, 0, 0, 0.867);
+  background-color: #FBF9F9;
+  padding: 5px;
+  width: 100%;
+  height: 24rem;
+  margin-bottom: 10px;
+  box-shadow: 8px 8px 3px rgba(0, 0, 0, 0.867);
 }
+
 #login_btn {
   font-weight: bold;
   cursor: pointer;
@@ -126,15 +141,17 @@ label, input {
   width: 17rem;
   margin: 3rem 0 0 8.5rem;
   font-size: 18px;
-  color: black;
+  color: navy;
   border: none;
-  box-shadow: 5px 5px 5px gray;
-  background: linear-gradient(to bottom right, #9E9CE2, #2e27e4); 
+  box-shadow: 5px 5px 5px rgb(163, 98, 98);
+  background: linear-gradient(to bottom right, #c6c6cf, #ababc2);
 }
+
 #login_btn:hover,
 #signup_btn:hover {
   filter: brightness(90%);
 }
+
 #signup_btn {
   font-weight: bold;
   font-style: normal;
@@ -142,42 +159,51 @@ label, input {
   border-radius: 20px;
   padding: 1rem;
   width: 17rem;
-  margin: 2.2rem 0 0 8.5rem; /*  12 for large monitor */
+  margin: 2.2rem 0 0 8.5rem;
+  /*  12 for large monitor */
   font-size: 18px;
-  color: black;
+  color: navy;
   border: none;
-  box-shadow: 5px 5px 5px gray;
-  background: linear-gradient(to bottom right, #7BDCA1, #05CD54); 
+  box-shadow: 5px 5px 5px rgb(163, 98, 98);
+  background: linear-gradient(to bottom right, #c6c6cf, #ababc2);
 }
+
 @media screen and (max-width: 768px) {
   .home {
     justify-content: center;
     flex-direction: column;
   }
+
   #logo {
     width: 250%;
     margin: 1rem;
   }
+
   .home_page_img {
     height: auto;
     margin: 1rem 0 0 1rem;
   }
+
   .login_form {
     margin: 1rem 1rem 0 1rem;
   }
+
   .login_section {
     width: 90%;
     margin: 1rem 0 0 1.25rem;
   }
+
   #login_card {
     height: 20rem;
     margin-bottom: 0;
     box-shadow: none;
   }
+
   #login_btn {
     margin: 1rem 0 0 1.25rem;
     box-shadow: none;
   }
+
   #signup_btn {
     margin: 1rem 0 0 1.25rem;
     box-shadow: none;
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,111 +1,115 @@
 <template>
-    <div>
-      <header-logo/>
-      <div class="col-md-4">
-        <form class ="signup_form">
-          <label for ="firstName-input" class = "form-label"></label>
-            <input type="text" class="form-control" id = "signup_firstName" placeholder="First Name" maxlength="15" v-model="firstName" required/> 
-          <label for ="lastName-input" class = "form-label"></label>
-            <input type = "text" class="form-control" id = "signup_lastName" placeholder="Last Name" maxlength = "20" v-model="lastName" required />
-          <label for ="email-input" class = "form-label"></label>
-            <input type = "text" class="form-control" id = "signup_email" placeholder= "Email" minlength="10" v-on:change="emailValidation()" v-model="email" required />
-          <label for = "password-input" class = "form-label"></label>
-            <input type = "text" class="form-control" id = "signup_password" placeholder = "Password" minlength="8" v-on:change="passwordValidation()" v-model="password" required/>
-          <router-link to = "/">
-            <input type="submit" id = "signup_btn_2" v-on:click ="SignUp()" value = "Sign Up"/>
-          </router-link>
-        </form>
-      </div>
+  <div>
+    <header-logo />
+    <div class="col-md-4">
+      <form class="signup_form">
+        <label for="firstName-input" class="form-label"></label>
+        <input type="text" class="form-control" id="signup_firstName" placeholder="First Name" maxlength="15"
+          v-model="firstName" required />
+        <label for="lastName-input" class="form-label"></label>
+        <input type="text" class="form-control" id="signup_lastName" placeholder="Last Name" maxlength="20"
+          v-model="lastName" required />
+        <label for="email-input" class="form-label"></label>
+        <input type="text" class="form-control" id="signup_email" placeholder="Email" minlength="10"
+          v-on:change="emailValidation()" v-model="email" required />
+        <label for="password-input" class="form-label"></label>
+        <input type="password" class="form-control" id="signup_password" placeholder="Password" minlength="8"
+          v-on:change="passwordValidation()" v-model="password" required />
+        <router-link to="/">
+          <input type="submit" id="signup_btn_2" v-on:click="SignUp()" value="Sign Up" />
+        </router-link>
+      </form>
     </div>
-  </template>
+  </div>
+</template>
   
   
-  <script>
-  import HeaderLogo from './HeaderLogo.vue';
-  export default {
-    name: "SignUp",
-    data() {
-      return {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        passConditions: {
-          email: false,
-          password: false
-        }
-      };
+<script>
+import HeaderLogo from './HeaderLogo.vue';
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passConditions: {
+        email: false,
+        password: false
+      }
+    };
+  },
+  watch: {
+    password() {
+      this.passwordValidation()
     },
-    watch: {
-      password() {
-        this.passwordValidation()
-      },
-      email() {
-        this.emailValidation()
+    email() {
+      this.emailValidation()
+    }
+  },
+
+  methods: {
+    passwordValidation() {
+      let password = this.password;
+      // console.log(password)
+      let passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      // console.log(passwordRegEx.test(password))
+      if (passwordRegEx.test(password)) {
+        this.passConditions.password = true
+        // console.log(this.passConditions)
+      } else {
+        this.passConditions.password = false
+      }
+      //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    },
+    emailValidation() {
+      let email = this.email;
+      let emailRegEx = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+      if (emailRegEx.test(email)) {
+        this.passConditions.email = true
+      } else {
+        this.passConditions.email = false
       }
     },
-   
-    methods: {
-      passwordValidation() {
-        let password = this.password;
-        // console.log(password)
-        let passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        // console.log(passwordRegEx.test(password))
-        if (passwordRegEx.test(password)) {
-          this.passConditions.password = true 
-          // console.log(this.passConditions)
-        } else {
-          this.passConditions.password = false
-        }
-        //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
-      },
-      emailValidation() {
-        let email = this.email;
-        let emailRegEx = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-        if (emailRegEx.test(email)) {
-          this.passConditions.email = true 
-        } else {
-          this.passConditions.email = false
-        }
-      },
-      SignUp() {
-        const signUpForm = {
-          firstName: this.firstName,
-          lastName: this.lastName,
-          email: this.email,
-          password: this.password
-        }
-        const requestOptions = {
-          method: "POST",
-          headers: {
+    SignUp() {
+      const signUpForm = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password
+      }
+      const requestOptions = {
+        method: "POST",
+        headers: {
           "Content-Type": "application/json",
-          },
-          body: JSON.stringify(signUpForm),
-        };
-        if (this.passConditions.password && this.passConditions.email === true) {
-          fetch("http://localhost:3000/api/users/signup", requestOptions)
-            .then((response) => {
-              return response.json();
-            })
-            .then((data) => {
-              console.log(data);
-              if (data.ok) {
-                // localStorage.setItem('token', res.data.token);
-                this.$router.push("/");
-              }
-            })
-            .catch((error) => {
-              console.error("There was an error!", error);
-            })
+        },
+        body: JSON.stringify(signUpForm),
+      };
+
+      fetch("http://localhost:3000/api/users/signup", requestOptions)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          if (data.ok) {
+            // localStorage.setItem('token', res.data.token);
+            this.$router.push("/");
           }
-        }
-      },
-    
-    components: { 
-      HeaderLogo
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        })
     }
+
+  },
+
+  components: {
+    HeaderLogo
   }
-  </script>
+}
+</script>
 
 <style scoped>
 * {
@@ -113,18 +117,22 @@
   padding: 0;
   margin: 0;
 }
-label, input {
+
+label,
+input {
   padding: .5rem;
   text-align: center;
   font-weight: 500;
   font-size: medium;
 }
+
 form {
   display: flex;
   flex-direction: column;
   width: 100%;
   margin: 6rem 0 0 16rem;
 }
+
 #signup_btn_2 {
   font-weight: bold;
   font-style: normal;
@@ -132,19 +140,20 @@ form {
   border-radius: 25px;
   padding: .5rem;
   width: 14rem;
-  margin: 4rem 0 0 9.7rem; 
+  margin: 4rem 0 0 9.7rem;
   font-size: 18px;
   color: black;
   border: none;
   box-shadow: 5px 5px 5px gray;
-  background: linear-gradient(to bottom right, #8521dd, #be5deb); 
+  background: linear-gradient(to bottom right, #8521dd, #be5deb);
 }
-    
+
 @media screen and (max-width: 768px) {
   form {
     width: 90%;
     margin: 1rem 0 0 1rem;
   }
+
   #signup_btn_2 {
     margin: 1rem 0 0 3rem;
     box-shadow: none;
