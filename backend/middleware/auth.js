99257b04eb16149aea3feb5c611880e6a161c219
MeredithBrandon <meredithbran2754@yahoo.com>
2023-01-31 20:05:46 -0500

    took out the error on line 14

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
         } else {
             next();
         }
-    } catch {
+    } catch(error) {
         res.status(401).json({
             error: new Error('Invalid request!')
         });
